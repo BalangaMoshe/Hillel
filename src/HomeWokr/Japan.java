ef94495253679a0f30a656201fea78d888f7cd58
Mykola Kovalets <kolya.kovalets@ukr.net>
2023-09-25 15:32:27 +0200

    Mykola Kovalets

--- src/HomeWokr/Japan.java
+++ src/HomeWokr/Japan.java
@@ -4,8 +4,8 @@ public class Japan {
 
     public static void main(String[] args) {
 
-        int i = 0;
-        while (i < 100) {
+      /*  int count = 0;
+        while (i <= 100) {
             i++;
 
             if (i / 10 == 4 || i % 10 == 4) {
@@ -14,8 +14,24 @@ public class Japan {
             if (i / 10 == 9 || i % 10 == 9) {
                 continue;
             }
+            count++;
             System.out.println("Номер шатлу " + i);
         }
-        System.out.println("Кількість пронумерованих " + i);
+        System.out.println("Кількість пронумерованих " + count); */
+
+        int counter = 0;
+        for (int i = 1; i <= 155; i++) {
+            if (i / 10 == 4 || i % 10 == 4 || i / 10 == 14) {
+                continue;
+            }
+            if (i / 10 == 9 || i % 10 == 9) {
+                continue;
+            }
+            counter++;
+            System.out.println("Номер шатлу " + i);
+        }
+        System.out.println("Кількість пронумерованих " + counter);
+
     }
 }
+
