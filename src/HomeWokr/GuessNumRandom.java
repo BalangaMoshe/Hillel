6cbf57a9aec26c3aeeff3c361bd9f63731b4e5e0
Mykola Kovalets <kolya.kovalets@ukr.net>
2023-09-28 11:03:49 +0200

    Mykola Kovalets

--- src/HomeWokr/GuessNumRandom.java
+++ src/HomeWokr/GuessNumRandom.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class GuessNumRandom {
     public static void main(String[] args) {
-     /*   int i = 5;
-        while (i >= 0) {
-
-            int random = (int) (Math.random() * 11);
-            System.out.println("Рандомне число " + random);
-            if (random >= 0 && random <= 10) {
-                System.out.println("WIIIINNNNN!!!!");
-                break;
-            }
-            System.out.println(i);
-            if (i == 0) {
-                System.out.println("BUUUUMMM!!!");
-            }
-            i--;
-       } */
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть будь яке число ");
+
+        for (int i = 0; i <= 3; i++) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Ви ввели не правильне число!");
+                scanner.nextLine();
+                if (i == 2) {
+                    System.out.println("Залишилась остання спроба!");
+                } else if (i == 3) {
+                    System.out.println("Спроби закінчено, спробуйте зпочатку!");
+                    System.exit(0);
+                }
+
+            }
+        }
+
 
         int random = (int) (Math.random() * 11);
 
