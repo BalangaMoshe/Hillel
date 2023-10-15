df800f8554aef641950cb182d6f734b04460b75c
Mykola Kovalets <kolya.kovalets@ukr.net>
2023-10-15 11:40:03 +0200

    Mykola Kovalets

--- src/HomeWokr/Burger.java
+++ src/HomeWokr/Burger.java
@@ -7,6 +7,8 @@ public class Burger {
         this.maio = maio;
         this.zwibel = zwibel;
         this.kese = kese;
+        System.out.println("ЗВИЧАЙНИЙ БУРГЕР(усі компоненти) *** ");
+
     }
     Burger(String brot, String fleish, String maio, String zwibel, String kese, String fleish2) {
         this.brot = brot;
@@ -15,6 +17,7 @@ public class Burger {
         this.zwibel = zwibel;
         this.kese = kese;
         this.fleish2 = fleish2;
+        System.out.println("ДІЄТИЧНИЙ БУРГЕР(без майонезу) *** ");
     }
 
      Burger(String brot, String fleish, String zwibel, String kese) {
@@ -22,10 +25,12 @@ public class Burger {
         this.fleish = fleish;
         this.zwibel = zwibel;
         this.kese = kese;
+         System.out.println("КОРОЛІВСЬКИЙ БУРГЕР(подвійне мясо) *** ");
     }
 
     public Burger(String zwibel) {
         this.zwibel = zwibel;
+        System.out.println("ВЄГАНСЬКИЙ(для похудєнія) *** ");
     }
 
     String brot;
@@ -36,19 +41,19 @@ public class Burger {
     String fleish2;
 
     String burgerNormal() {
-        return "ЗВИЧАЙНИЙ БУРГЕР(усі компоненти) *** " + brot + fleish + maio + zwibel + kese + "***";
+        return brot + fleish + maio + zwibel + kese;
     }
 
     String burgerDiette() {
-        return "ДІЄТИЧНИЙ БУРГЕР(без майонезу) *** " + brot + fleish + zwibel + kese + "***";
+        return  brot + fleish + zwibel + kese;
     }
 
     String burgerTooFleish() {
-        return "КОРОЛІВСЬКИЙ БУРГЕР(подвійне мясо) *** " + brot + fleish + maio + zwibel + kese + fleish + "***";
+        return brot + fleish + maio + zwibel + kese + fleish;
     }
 
     String burgerVegan() {
-        return "ВЄГАНСЬКИЙ(для похудєнія) *** " + zwibel + "***";
+        return  zwibel;
     }
 
 }
